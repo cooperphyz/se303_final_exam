f37586f3e93f01f4337771ce5abf8c838f45e18d
cooperphyz <gjcooper99@gmail.com>
2022-06-07 10:48:01 -0700

    Use new middlespace function

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -4,11 +4,7 @@
 def story
   first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
   middle = ""
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
+  middle << middlespace
   conclusion = "And that was it."
   first + middle + conclusion
 end
